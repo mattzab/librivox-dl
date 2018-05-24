4f42a01b6029517955b372cb36900a170bdaa1da
mattzab <mattzab@gmail.com>
2018-05-24 10:41:31 -0600

    Update continue-download.sh

--- continue-download.sh
+++ continue-download.sh
@@ -1,2 +1,4 @@
-while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < list1.xml
-while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < list2.xml
+while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < "~/Librivox Download/list1.xml"
+while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < "~/Librivox Download/list2.xml"
+while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < "~/Librivox Download/list3.xml"
+while read a; read b; read c; read d; read e; do wget -c $c -O $b$e$d$a; echo $c; done < "~/Librivox Download/list4.xml"
